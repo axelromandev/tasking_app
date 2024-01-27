032ef9481d673f6943d482d5b7d6a4e8b3e5f9b8
Axel Roman <ingedevs@outlook.com>
2024-01-27 06:49:24 -0600

    :fire: Remove google ads

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -1,3 +1,2 @@
-export 'ad_mod_service.dart';
 export 'isar_service.dart';
 export 'shared_prefs_service.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await IsarService.initialize();
   await SharedPrefsService.initialize();
-  await AdModService.configure();
   runApp(const ProviderScope(
     child: MainApp(),
   ));
