739d5f37761a574f9d08c458322da57951bcd777
Axel Roman <ingedevs@outlook.com>
2024-04-28 02:03:54 -0600

    ✨ feat: Add crypto library for generating nonce and SHA-256

--- lib/core/providers/auth_provider.dart
+++ lib/core/providers/auth_provider.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
+import 'dart:math' as m;
 
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -61,25 +64,30 @@ class _Notifier extends StateNotifier<_State> {
 
   Future<UserCredential?> signInWithApple() async {
     try {
+      final rawNonce = _generateNonce();
+      final nonce = _sha256ofString(rawNonce);
+      final webAuthenticationOptions = WebAuthenticationOptions(
+        clientId: 'com.ingedevs.tasking-service',
+        redirectUri: Uri.parse(
+          'https://callbackssigninwithapple-ghl66f62mq-uc.a.run.app',
+        ),
+      );
       UserCredential? credential;
-      if (Platform.isAndroid) {
-        credential = await FirebaseAuth.instance.signInWithProvider(
-          AppleAuthProvider(),
-        );
-      } else {
-        final apple = await SignInWithApple.getAppleIDCredential(
-          scopes: [
-            AppleIDAuthorizationScopes.email,
-            AppleIDAuthorizationScopes.fullName,
-          ],
-        );
-        credential = await FirebaseAuth.instance.signInWithCredential(
-          OAuthProvider('apple.com').credential(
-            idToken: apple.identityToken,
-            accessToken: apple.authorizationCode,
-          ),
-        );
-      }
+      final apple = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+        nonce: Platform.isIOS ? nonce : null,
+        webAuthenticationOptions:
+            Platform.isIOS ? null : webAuthenticationOptions,
+      );
+      final provider = OAuthProvider('apple.com').credential(
+        idToken: apple.identityToken,
+        rawNonce: Platform.isIOS ? rawNonce : null,
+        accessToken: Platform.isIOS ? null : apple.authorizationCode,
+      );
+      credential = await FirebaseAuth.instance.signInWithCredential(provider);
       await _prefs.setKeyValue<int>(
           Keys.authProvider, AuthProvider.apple.index);
       state = state.copyWith(provider: AuthProvider.apple);
@@ -87,6 +95,28 @@ class _Notifier extends StateNotifier<_State> {
     } catch (e) {
       log('$e', name: 'onSignInWithApple');
       return null;
+    }
+  }
+
+  String _generateNonce([int length = 32]) {
+    try {
+      const charset =
+          '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+      final random = m.Random.secure();
+      return List.generate(
+          length, (_) => charset[random.nextInt(charset.length)]).join();
+    } catch (e) {
+      throw Exception('Error generateNonce: $e');
+    }
+  }
+
+  String _sha256ofString(String input) {
+    try {
+      final bytes = utf8.encode(input);
+      final digest = sha256.convert(bytes);
+      return digest.toString();
+    } catch (e) {
+      throw Exception('Error sha256ofString: $e');
     }
   }
 
