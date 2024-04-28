dbcf7384bf9648da0901487f546c94eaeb189764
Axel Roman <ingedevs@outlook.com>
2024-04-28 02:10:12 -0600

    ✨ feat: Add .env to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -10,13 +10,10 @@ app.post('/', (req, res) => {
   const bodyParams = new URLSearchParams(req.body).toString();
 
   // Replace with your Android package identifier
-  const ANDROID_PACKAGE_IDENTIFIER = 'com.ingedevs.tasking';
+  const ANDROID_PACKAGE_IDENTIFIER = process.env.ANDROID_PACKAGE_IDENTIFIER;
 
   // Replace with your Android package identifier
   const redirect = `intent://callback?${bodyParams}#Intent;package=${ANDROID_PACKAGE_IDENTIFIER};scheme=signinwithapple;end`;
-
-  // Log the redirect URL
-  console.log(`Redirecting to ${redirect}`);
 
   // Redirect to the Android app
   res.redirect(307, redirect);
