c30e8898427b83cf1083fa4c36ab8bcba259d494
Axel Roman <ingedevs@outlook.com>
2024-10-03 17:33:56 -0600

    ✨ feat: Integrate recent tasks into HomeView design

--- lib/features/presentation/pages/views/home_view.dart
+++ lib/features/presentation/pages/views/home_view.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:tasking/config/config.dart';
+import 'package:tasking/features/presentation/providers/providers.dart';
 import 'package:tasking/i18n/i18n.dart';
 
 class HomeView extends ConsumerWidget {
@@ -14,6 +16,7 @@ class HomeView extends ConsumerWidget {
     final style = Theme.of(context).textTheme;
 
     final colorPrimary = ref.watch(colorThemeProvider);
+    final tasks = ref.read(listsProvider.notifier).getRecentTasks();
 
     return Scaffold(
       appBar: AppBar(
@@ -38,7 +41,72 @@ class HomeView extends ConsumerWidget {
         ),
         centerTitle: false,
       ),
-      body: const Center(child: Text('HomeView')),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text(
+              'Yuhuu, ¡tu trabajo casi está terminado!',
+              style: style.titleLarge,
+            ),
+          ),
+          const Gap(defaultPadding),
+          Card(
+            margin: const EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: ListTile(
+              title: const Text('Tareas diarias'),
+              subtitle: const Text(
+                '8 de 10 Completados',
+                style: TextStyle(color: Colors.grey),
+              ),
+              trailing: SizedBox(
+                width: 40,
+                height: 40,
+                child: Stack(
+                  children: [
+                    const Center(
+                      child: CircularProgressIndicator(
+                        value: 0.8,
+                        strokeWidth: 3,
+                        color: Colors.white,
+                        backgroundColor: Colors.white12,
+                      ),
+                    ),
+                    Center(
+                      child: Text('80%', style: style.labelSmall),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Gap(defaultPadding),
+          if (tasks.isNotEmpty)
+            Column(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(left: defaultPadding),
+                  child: const Text('Recientes'),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: tasks.length,
+                  itemBuilder: (_, i) {
+                    final task = tasks[i];
+                    return ListTile(
+                      visualDensity: VisualDensity.compact,
+                      leading: const Icon(BoxIcons.bx_circle),
+                      title: Text(task.title),
+                    );
+                  },
+                ),
+              ],
+            )
+          else
+            const Text('Libre por hoy :D'),
+        ],
+      ),
     );
   }
 }
