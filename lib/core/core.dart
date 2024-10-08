f24d2163d1c2985ea9177f0360997d40da0fe5c6
Axel Roman <ingedevs@outlook.com>
2024-10-08 14:59:43 -0600

    🧹 chore(tools): remove datetime_picker utility

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -2,7 +2,6 @@ export './extensions/datetime.dart';
 export './helpers/database_helper.dart';
 export './services/notification_service.dart';
 export './services/shared_prefs_service.dart';
-export './utils/datetime_picker.dart';
 export './utils/debouncer.dart';
 export './utils/human_format.dart';
 export './utils/icon_data_utils.dart';
